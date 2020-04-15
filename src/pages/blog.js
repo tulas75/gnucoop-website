01f7392c7ff03211ca4c7b7c6e50ac30e3e43175
Roberto Zanotto <roberto.zanotto@gnucoop.com>
2020-04-15 14:41:33 +0200

    blog page updated

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -7,49 +7,50 @@ import Layout from '../components/layout'
 import HeaderGeneric from '../components/HeaderGeneric'
 import Nav from '../components/Nav'
 
-    const BlogPage = ({ data }) => (
-      <Layout>
-        <Nav sticky={true} />
-        <Helmet title="Blog & Articles" />
-        <header id="header">
-        <h1>Blog & Articles</h1>
-        <p>News from us</p>
+const BlogPage = ({ data }) => (
+  <Layout>
+    <Nav sticky={true} />
+    <Helmet title="Blog & Articles" />
+    <header id="header">
+      <h1>Blog & Articles</h1>
+      <p>News from us</p>
     </header>
-        <div id="main">
-        <section class="main">
+    <div id="main">
+      <section class="main">
         <ul class="features">
-	  {data.allStrapiStuff.edges.map(document => (
-            <li key={document.node.id}>
-            <span className="main image"><Img fixed={document.node.feature_image.childImageSharp.fixed}/>
-            </span>
-                <a href={`/${document.node.slug}`}>{document.node.title}</a>
+          {data.allStrapiArticles.edges.map(article => (
+            <li key={article.node.id}>
+              <span className="main image">
+                <Img fixed={article.node.feature_image.childImageSharp.fixed}/>
+              </span>
+              <a href={`/${article.node.slug}`}>{article.node.title}</a>
             </li>
-	  ))}
+          ))}
         </ul>
-		</section>
-        </div>
-      </Layout>
-    )
+		  </section>
+    </div>
+  </Layout>
+)
 
 export default BlogPage 
 
 export const pageQuery = graphql`
   query BlogQuery {
-    allStrapiStuff(filter: {category: {category: {eq: "Blog"}}}) {
+    allStrapiArticles() {
       edges {
         node {
           id
-	  feature_image {
-	      childImageSharp {
+          feature_image {
+            childImageSharp {
               fixed(width: 300, height: 200) {
                 ...GatsbyImageSharpFixed
               }
             }
-	  }
+          }
           title
-     	  slug
+     	    slug
+        }
       }
-    }
     }
   }
 `
