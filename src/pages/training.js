271abe5415ad975125b1c5ea27b386a6d6b684bf
Roberto Zanotto <roberto.zanotto@gnucoop.com>
2020-04-15 14:53:18 +0200

    training page updated

--- src/pages/training.js
+++ src/pages/training.js
@@ -5,58 +5,58 @@ import Layout from '../components/layout'
 import HeaderGeneric from '../components/HeaderGeneric'
 import Nav from '../components/Nav'
 
-
 const TrainingPage = ({ data }) => (
-      <Layout>
-        <Nav sticky={true} />
-        <header id="header">
-        <h1>Our training</h1>
-        <p> Education is the most powerful weapon<br/> which you can use to change the world</p>
+  <Layout>
+    <Nav sticky={true} />
+    <header id="header">
+      <h1>Our training</h1>
+      <p>Education is the most powerful weapon<br/> which you can use to change the world</p>
     </header>
-        <Helmet title="Our training" />
-        <div id="main">
-          <section id="content" className="main">
-		            <p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fergiat. Pellentesque in mi eu massa lacinia malesuada et a elit. Donec urna ex, lacinia in purus ac, pretium pulvinar mauris. Curabitur sapien risus, commodo eget turpis at, elementum convallis fames ac ante ipsum primis in faucibus.</p>
-            <p>Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Consequat leo mauris, consectetur id ipsum sit amet, fersapien risus, commodo eget turpis at, elementum convallis elit enim turpis lorem ipsum dolor sit amet feugiat. Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus, lacus eget hendrerit bibendum, urna est aliquam sem, sit amet est velit quis lorem.</p>
-            <h2>Tempus veroeros</h2>
-            <p>Cep risus aliquam gravida cep ut lacus amet. Adipiscing faucibus nunc placerat. Tempus adipiscing turpis non blandit accumsan eget lacinia nunc integer interdum amet aliquam ut orci non col ut ut praesent. Semper amet interdum mi. Phasellus enim laoreet ac ac commodo faucibus faucibus. Curae ante vestibulum ante. Blandit. Ante accumsan nisi eu placerat gravida placerat adipiscing in risus fusce vitae ac mi accumsan nunc in accumsan tempor blandit aliquet aliquet lobortis. Ultricies blandit lobortis praesent turpis. Adipiscing accumsan adipiscing adipiscing ac lacinia cep. Orci blandit a iaculis adipiscing ac. Vivamus ornare laoreet odio vis praesent nunc lorem mi. Erat. Tempus sem faucibus ac id. Vis in blandit. Nascetur ultricies blandit ac. Arcu aliquam. Accumsan mi eget adipiscing nulla. Non vestibulum ac interdum condimentum semper commodo massa arcu.</p>
-<h1>Training</h1>
-          <ul class="features">
-          {data.allStrapiStuff.edges.map(document => (
-            <li key={document.node.id}>
-            <span className="main image"><Img fixed={document.node.feature_image.childImageSharp.fixed}/></span>
-                <strong>{document.node.title}</strong><br/>
-                {document.node.excerpt}
+    <Helmet title="Our training" />
+    <div id="main">
+      <section id="content" className="main">
+        <p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fergiat. Pellentesque in mi eu massa lacinia malesuada et a elit. Donec urna ex, lacinia in purus ac, pretium pulvinar mauris. Curabitur sapien risus, commodo eget turpis at, elementum convallis fames ac ante ipsum primis in faucibus.</p>
+        <p>Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Consequat leo mauris, consectetur id ipsum sit amet, fersapien risus, commodo eget turpis at, elementum convallis elit enim turpis lorem ipsum dolor sit amet feugiat. Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus, lacus eget hendrerit bibendum, urna est aliquam sem, sit amet est velit quis lorem.</p>
+        <h2>Tempus veroeros</h2>
+        <p>Cep risus aliquam gravida cep ut lacus amet. Adipiscing faucibus nunc placerat. Tempus adipiscing turpis non blandit accumsan eget lacinia nunc integer interdum amet aliquam ut orci non col ut ut praesent. Semper amet interdum mi. Phasellus enim laoreet ac ac commodo faucibus faucibus. Curae ante vestibulum ante. Blandit. Ante accumsan nisi eu placerat gravida placerat adipiscing in risus fusce vitae ac mi accumsan nunc in accumsan tempor blandit aliquet aliquet lobortis. Ultricies blandit lobortis praesent turpis. Adipiscing accumsan adipiscing adipiscing ac lacinia cep. Orci blandit a iaculis adipiscing ac. Vivamus ornare laoreet odio vis praesent nunc lorem mi. Erat. Tempus sem faucibus ac id. Vis in blandit. Nascetur ultricies blandit ac. Arcu aliquam. Accumsan mi eget adipiscing nulla. Non vestibulum ac interdum condimentum semper commodo massa arcu.</p>
+        <h1>Training</h1>
+        <ul class="features">
+          {data.allStrapiTraining.edges.map(training => (
+            <li key={training.node.id}>
+              <span className="main image">
+                <Img fixed={training.node.feature_image.childImageSharp.fixed}/>
+              </span>
+              <strong>{training.node.title}</strong>
+              <br/>
+              {training.node.excerpt}
             </li>
           ))}
         </ul>
-          </section>
-        </div>
-      </Layout>
-	    )
+      </section>
+    </div>
+  </Layout>
+)
 
 export default TrainingPage 
 
-
 export const pageQuery = graphql`
   query TrainingQuery {
-    allStrapiStuff(filter: {category: {category: {eq: "Training"}}}) {
-       edges {	
-	       node {
-		id
-		title
-		excerpt
-		slug
-          	feature_image {
-              childImageSharp {
+    allStrapiTraining() {
+      edges {  
+        node {
+          id
+          title
+          excerpt
+          slug
+          feature_image {
+            childImageSharp {
               fixed(width: 260, height: 260) {
                 ...GatsbyImageSharpFixed
               }
             }
           }
+        }
       }
     }
-	  }
   }
 `
-
