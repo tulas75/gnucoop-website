89d6da3da9e799815ca55df51c26ce4bd54f716c
Roberto Zanotto <roberto.zanotto@gnucoop.com>
2020-06-30 15:44:51 +0200

    nav mobile breakpoint

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,30 +1,31 @@
 import React from 'react'
-import Scrollspy from 'react-scrollspy'
 import { Link } from 'gatsby'
 import pic01 from '../assets/images/logo.png'
 
 const Nav = (props) => (
   <nav id="nav" className={props.sticky ? 'alt' : ''}>
-    <Scrollspy currentClassName="is-active">
+    <ul>
       <li>
-        <span className="image left"><img src={pic01} alt="" /></span>
+        <img src={pic01} alt=""
+          style={{width: '33.6px', height: '30.2px', margin: '0 20px'}} />
       </li>
       <li>
-        <Link to="/" type="id" element="who-we-are">Home</Link>
+        <Link to="/">Home</Link>
       </li>
       <li >
-        <Link to="/who-we-are" type="id" element="who-we-are">Who we are</Link>
+        <Link to="/who-we-are">Who we are</Link>
+      </li>
+      <br /><hr />
+      <li>
+        <Link to="/what-we-do">What we do</Link>
       </li>
       <li>
-        <Link to="/what-we-do" type="id" element="what-we-do">What we do</Link>
+        <Link to="/blog">Blog</Link>
       </li>
       <li>
-        <Link to="/blog" type="id" element="what-we-do">Blog</Link>
+        <Link to="/training">Training</Link>
       </li>
-      <li>
-        <Link to="/training" type="id" element="academy">Training</Link>
-      </li>
-    </Scrollspy>
+    </ul>
   </nav>
 )
 
