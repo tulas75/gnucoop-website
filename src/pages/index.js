271c23e38111f4c93593eeebd5104b1ec6e4edf6
Roberto Zanotto <roberto.zanotto@gnucoop.com>
2020-07-06 14:50:20 +0200

    homepage fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -112,12 +112,12 @@ class Index extends React.Component {
           <section id="projects-partners" className="main special">
             <header className="major">
               <h2>Our Projects with our Partners</h2>
-              <p>
-                Donec imperdiet consequat consequat. Suspendisse feugiat congue
-                <br />
-                posuere. Nulla massa urna, fermentum eget quam aliquet.
-              </p>
             </header>
+            <p>
+              We support NGO and non-profit organisations in their work. We have experience in several fields of development, from education to agriculture.
+              We use effective and sustainable technological solutions that are already available or, when necessary, we develop our own.
+              If you have a problem, together we can think of a solution!
+            </p>
             <ul className="statistics">
               <li className="style1">
                 <span className="icon fa-code-fork"></span>
@@ -140,18 +140,6 @@ class Index extends React.Component {
                 <strong>CUAMM</strong> Data analytis to fight HIV
               </li>
             </ul>
-            <p className="content">
-              Nam elementum nisl et mi a commodo porttitor. Morbi sit amet nisl
-              eu arcu faucibus hendrerit vel a risus. Nam a orci mi, elementum
-              ac arcu sit amet, fermentum pellentesque et purus. Integer maximus
-              varius lorem, sed convallis diam accumsan sed. Etiam porttitor
-              placerat sapien, sed eleifend a enim pulvinar faucibus semper quis
-              ut arcu. Ut non nisl a mollis est efficitur vestibulum. Integer
-              eget purus nec nulla mattis et accumsan ut magna libero. Morbi
-              auctor iaculis porttitor. Sed ut magna ac risus et hendrerit
-              scelerisque. Praesent eleifend lacus in lectus aliquam porta. Cras
-              eu ornare dui curabitur lacinia.
-            </p>
             <footer className="major">
               <ul className="actions">
                 <li>
