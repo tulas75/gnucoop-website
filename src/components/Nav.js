9158d42a013c42296005cea4b2b024031a3b522a
Roberto Zanotto <roberto.zanotto@gnucoop.com>
2020-07-23 18:59:56 +0200

    button for mobile nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,27 +1,32 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import pic01 from '../assets/images/logo.png'
+import logo from '../assets/images/logo.png'
+import menu from '../assets/images/menu.svg'
 
 const Nav = (props) => (
   <nav id="nav" className={props.sticky ? 'alt' : ''}>
     <ul>
-      <li>
-        <img src={pic01} alt=""
+      <li className="mobileNav">
+        <img src={menu} alt=""
+          style={{width: '30px', height: '30px', margin: '0 20px'}} />
+      </li>
+      <li class="desktopNav">
+        <img src={logo} alt=""
           style={{width: '33.6px', height: '30.2px', margin: '0 20px'}} />
       </li>
-      <li>
+      <li className="desktopNav">
         <Link to="/">Home</Link>
       </li>
-      <li >
+      <li className="desktopNav">
         <Link to="/who-we-are">Who we are</Link>
       </li>
-      <li>
+      <li className="desktopNav">
         <Link to="/what-we-do">What we do</Link>
       </li>
-      <li>
+      <li className="desktopNav">
         <Link to="/blog/1">Blog</Link>
       </li>
-      <li>
+      <li className="desktopNav">
         <Link to="/training">Training</Link>
       </li>
     </ul>
