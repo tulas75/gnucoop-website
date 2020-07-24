c4cc39fb25a2747cb3dc4ca70a4bff7199f7504a
Roberto Zanotto <roberto.zanotto@gnucoop.com>
2020-07-24 17:02:27 +0200

    first version of mobile nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,34 +3,78 @@ import { Link } from 'gatsby'
 import logo from '../assets/images/logo.png'
 import menu from '../assets/images/menu.svg'
 
-const Nav = (props) => (
-  <nav id="nav" className={props.sticky ? 'alt' : ''}>
-    <ul>
-      <li className="mobileNav">
-        <img src={menu} alt=""
-          style={{width: '30px', height: '30px', margin: '0 20px'}} />
-      </li>
-      <li class="desktopNav">
-        <img src={logo} alt=""
-          style={{width: '33.6px', height: '30.2px', margin: '0 20px'}} />
-      </li>
-      <li className="desktopNav">
-        <Link to="/">Home</Link>
-      </li>
-      <li className="desktopNav">
-        <Link to="/who-we-are">Who we are</Link>
-      </li>
-      <li className="desktopNav">
-        <Link to="/what-we-do">What we do</Link>
-      </li>
-      <li className="desktopNav">
-        <Link to="/blog/1">Blog</Link>
-      </li>
-      <li className="desktopNav">
-        <Link to="/training">Training</Link>
-      </li>
-    </ul>
-  </nav>
-)
+class Nav extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { mobileNav: false }
+    this.props = props
+  }
+
+  showMobileNav = () => {
+    this.setState(() => ({ mobileNav: true }))
+  }
+
+  hideMobileNav = () => {
+    this.setState(() => ({ mobileNav: false }))
+  }
+
+  render() {
+    return (
+      <div>
+        <nav id="nav" className={this.props.sticky ? 'alt' : ''}>
+          <ul>
+            <li className="mobileNav">
+              <img src={menu} alt="" onClick={this.showMobileNav}
+                style={{width: '30px', height: '30px', margin: '0 20px'}} />
+            </li>
+            <li class="desktopNav">
+              <img src={logo} alt=""
+                style={{width: '33.6px', height: '30.2px', margin: '0 20px'}} />
+            </li>
+            <li className="desktopNav">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="desktopNav">
+              <Link to="/who-we-are">Who we are</Link>
+            </li>
+            <li className="desktopNav">
+              <Link to="/what-we-do">What we do</Link>
+            </li>
+            <li className="desktopNav">
+              <Link to="/blog/1">Blog</Link>
+            </li>
+            <li className="desktopNav">
+              <Link to="/training">Training</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <div id="mobileNavBg" onClick={this.hideMobileNav}
+            className={this.state.mobileNav ? 'visible' : ''} />
+        
+        <nav id="mobileNav"
+            className={this.state.mobileNav ? 'visible' : ''}>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/who-we-are">Who we are</Link>
+            </li>
+            <li>
+              <Link to="/what-we-do">What we do</Link>
+            </li>
+            <li>
+              <Link to="/blog/1">Blog</Link>
+            </li>
+            <li>
+              <Link to="/training">Training</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    )
+  }
+}
 
 export default Nav
