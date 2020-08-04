c3297ec95b1ff9086a96ebfef67aa3fa119f88c0
tulas <andrea.bertolazzi@gnucoop.com>
2020-08-04 18:36:52 +0200

    add projects pages

---
+++ src/templates/project.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+import Img from 'gatsby-image'
+
+import Layout from '../components/layout'
+import Nav from '../components/Nav'
+import ReactMarkdown from 'react-markdown'
+import { formatDateEu } from '../utils/utils'
+
+const ProjectTemplate = ({ data }) => (
+  <Layout>
+    <Nav sticky={true} />
+    <Helmet title={'Gnucoop - '+data.strapiProjects.Project} />
+    <div id="main">
+      <section id="content" className="main">
+        <span className="main image ">
+          <Img fluid={data.strapiProjects.FeatureImage.childImageSharp.fluid}/>
+        </span>
+        <h1>{data.strapiProjects.Project}</h1>
+	      <ReactMarkdown source={data.strapiProjects.Content} />
+	      <br/>
+      </section>
+    </div>
+  </Layout>
+)
+
+export default ProjectTemplate
+
+export const query = graphql`
+  query ProjectTemplate($Slug: String) {
+    strapiProjects(Slug: {eq: $Slug}) {
+      Project 
+      Content
+      FeatureImage {
+        childImageSharp {
+          fluid(maxHeight: 450) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      Slug
+    }
+  }
+`
