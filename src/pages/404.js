0cd608a75850dba9d3b1adedad2e47dc16b111d1
tulas <andrea.bertolazzi@gnucoop.com>
2020-08-08 18:54:27 +0200

    add 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 import Layout from '../components/layout'
+import Img from 'gatsby-image'
+import notfound from '../assets/images/404.png'
+import Nav from '../components/Nav'
 
 const NotFoundPage = () => (
   <Layout>
+    <Nav sticky={true} />
     <Helmet title="Gnucoop - 404" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <header id="header">
+      <h1>404 Not found</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+              <span class="main image">
+                <img src={notfound} alt="" />
+              </span>
+    </header>
+
   </Layout>
 )
 
