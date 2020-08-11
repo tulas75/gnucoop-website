aa9577631e955cbee0b0a525f2bfd5f2511b0581
Roberto Zanotto <roberto.zanotto@gnucoop.com>
2020-08-11 15:50:24 +0200

    mailchimp integration

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,65 +1,85 @@
 import React from 'react'
+import addToMailchimp from 'gatsby-plugin-mailchimp'
 
-const Footer = props => (
-  <footer id="footer">
-    <section>
-      <h2>Get in touch!</h2>
-        <form method="post" action="#">
-          <label>
-            Name <input type="text" name="name" id="name" />
-          </label>
-          <label>
-            Email <input type="email" name="email" id="email" />
-          </label>
-          <button type="submit">Send</button>
-        </form>
-    </section>
-    <section>
-      <h2>Contacts</h2>
-      <dl className="alt">
-        <dt>Address</dt>
-        <dd>via Stefanardo da Vimercate 28 &bull; <br />Milano, 20128 &bull; ITALY</dd>
-        <dt>Phone</dt>
-        <dd>(+39) 370-3309574</dd>
-        <dt>Address</dt>
-        <dd>Lungarno Mediceo 56 &bull; Pisa, 56127 &bull; ITALY</dd>
-        <dt>Phone</dt>
-        <dd>(+39) 370-3173931</dd>
-        <dt>Email</dt>
-        <dd><a href="mailto:info@gnucoop.com">info@gnucoop.com</a></dd>
-      </dl>
-      <ul className="icons">
-        <li>
-          <a href="https://twitter.com/gnucoop" className="icon fa-twitter alt">
-            <span className="label">Twitter</span>
-          </a>
-        </li>
-        <li>
-          <a href="https://facebook.com/gnucoop" className="icon fa-facebook alt">
-            <span className="label">Facebook</span>
-          </a>
-        </li>
-        <li>
-          <a href="https://instagram.com/gnucoop_soc_coop" className="icon fa-instagram alt">
-            <span className="label">Instagram</span>
-          </a>
-        </li>
-        <li>
-          <a href="https://www.youtube.com/channel/UCtY0iTvoG10nDospIa34qzQ" className="icon fa-youtube alt">
-            <span className="label">YouTube</span>
-          </a>
-        </li>
-        <li>
-          <a href="https://github.com/gnucoop" className="icon fa-github alt">
-            <span className="label">GitHub</span>
-          </a>
-        </li>
-      </ul>
-    </section>
-    <p className="copyright">
-      &copy; Gnucoop Soc. Coop. Definetely inspired by <a href="https://html5up.net">HTML5 UP</a>.
-    </p>
-  </footer>
-)
+class Footer extends React.Component {
+
+  submitMailchimp = e => {
+    e.preventDefault();
+    const email = document.getElementById('subEmail').value;
+    const name = document.getElementById('subName').value;
+    addToMailchimp(email, {MERGE1: name, MERGE4: 'English'})
+    .then(data => {
+      if (data.result === 'success') {
+        window.alert('Thank you for subscribing :)');
+      } else {
+        console.log(data)
+      }
+    });
+  }
+
+  render() {
+    return (
+      <footer id="footer">
+        <section>
+          <h2>Subscribe to our newsletter!</h2>
+            <form>
+              <label>
+                Name <input type="text" name="name" id="subName" />
+              </label>
+              <label>
+                Email <input type="email" name="email" id="subEmail" />
+              </label>
+              <button onClick={this.submitMailchimp}>Send</button>
+            </form>
+        </section>
+        <section>
+          <h2>Contacts</h2>
+          <dl className="alt">
+            <dt>Address</dt>
+            <dd>via Stefanardo da Vimercate 28 &bull; <br />Milano, 20128 &bull; ITALY</dd>
+            <dt>Phone</dt>
+            <dd>(+39) 370-3309574</dd>
+            <dt>Address</dt>
+            <dd>Lungarno Mediceo 56 &bull; Pisa, 56127 &bull; ITALY</dd>
+            <dt>Phone</dt>
+            <dd>(+39) 370-3173931</dd>
+            <dt>Email</dt>
+            <dd><a href="mailto:info@gnucoop.com">info@gnucoop.com</a></dd>
+          </dl>
+          <ul className="icons">
+            <li>
+              <a href="https://twitter.com/gnucoop" className="icon fa-twitter alt">
+                <span className="label">Twitter</span>
+              </a>
+            </li>
+            <li>
+              <a href="https://facebook.com/gnucoop" className="icon fa-facebook alt">
+                <span className="label">Facebook</span>
+              </a>
+            </li>
+            <li>
+              <a href="https://instagram.com/gnucoop_soc_coop" className="icon fa-instagram alt">
+                <span className="label">Instagram</span>
+              </a>
+            </li>
+            <li>
+              <a href="https://www.youtube.com/channel/UCtY0iTvoG10nDospIa34qzQ" className="icon fa-youtube alt">
+                <span className="label">YouTube</span>
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/gnucoop" className="icon fa-github alt">
+                <span className="label">GitHub</span>
+              </a>
+            </li>
+          </ul>
+        </section>
+        <p className="copyright">
+          &copy; Gnucoop Soc. Coop. Definetely inspired by <a href="https://html5up.net">HTML5 UP</a>.
+        </p>
+      </footer>
+    )
+  }
+}
 
 export default Footer
