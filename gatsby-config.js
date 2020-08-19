a688e8612050f649a786b405b1f6f0faae267e0c
trik <marco.marche@gnucoop.com>
2020-08-19 13:15:08 +0200

    chore: set higher google analytics sample rate

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,7 +70,7 @@ module.exports = {
         reactGaOptions: {
             debug: false,
             gaOptions: {
-                sampleRate: 10
+                sampleRate: 100
             }
         }
       },
