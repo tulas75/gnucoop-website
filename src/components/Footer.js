c4c8626e36b2439f8977d56a60624341295b15d9
Roberto Zanotto <roberto.zanotto@gnucoop.com>
2021-01-13 10:47:50 +0100

    chatbot moved from index to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,6 +13,17 @@ class Footer extends React.Component {
 
   componentDidMount() {
     this.setState(() => ({ cookiesBar: getCookie(acceptedCookie) !== 'true' }))
+
+    // Chatbot:
+    window.tiledeskSettings = { projectid: "5feb303df5195b0019949897" };
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s);
+      js.id = id;
+      js.src = "https://widget.tiledesk.com/v4/launch.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'tiledesk-jssdk'));
   }
 
   acceptCookies = e => {
